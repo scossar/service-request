bbdfafb9aefd8461ca5c6a234e5885ca6ec173b4
scossar <scossar3@gmail.com>
2015-10-15 10:42:35 -0700

    align radio buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,8 @@
 (function($) {
   'use strict';
   $('.radio-button').click(function(){
-    $(this).find('.radio').toggleClass('radio-hide radio-display');
-    $(this).siblings('.radio-button').find('.radio').toggleClass('radio-hide radio-display');
+    //$(this).find('.radio').toggleClass('radio-hide radio-display');
+    //$(this).siblings('.radio-button').find('.radio').toggleClass('radio-hide radio-display');
 
     $('#discourse-entry').find('.form').toggleClass('hidden');
   });
